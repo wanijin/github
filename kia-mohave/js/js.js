e6fcda7d6be4a92f18b2e27cf2812b7d0e0ad9c3
sujeoung00 <sujeoung00@naver.com>
2020-07-13 12:01:41 +0900

    kia

--- kia-mohave/js/js.js
+++ kia-mohave/js/js.js
@@ -245,6 +245,19 @@ $(function(){
 });
 
 
+var video = document.getElementById("kiaVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+};
+
 
 
 
