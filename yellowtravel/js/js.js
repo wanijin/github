2f618a07a8ab9efa4eeda3ef9ea1d79edfc1f3dc
wanijin <65152217+wanijin@users.noreply.github.com>
2020-08-10 13:27:14 +0900

    Add files via upload

--- yellowtravel/js/js.js
+++ yellowtravel/js/js.js
@@ -10,7 +10,50 @@ function openPage(pageName,elmnt,color) {
     }
     document.getElementById(pageName).style.display = "block";
     elmnt.style.backgroundColor = color;
+    elmnt.style.color = '#181818';
   }
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+
+
+
+  // index > sevice-icon icon & textcolor hover
+  
+  $('.service-icon>div:nth-child(1)').mouseover(function(){
+    $('.service-icon>div:nth-child(1) img').attr('src','images/icon-rome-y.png');
+    $('.service-icon>div:nth-child(1) a').css('color','#ffd800');
+  });
+  $('.service-icon>div:nth-child(1)').mouseleave(function(){
+    $('.service-icon>div:nth-child(1) img').attr('src','images/icon-rome.png');
+    $('.service-icon>div:nth-child(1) a').css('color','#fff');
+  });
+
+  $('.service-icon>div:nth-child(2)').mouseover(function(){
+    $('.service-icon>div:nth-child(2) img').attr('src','images/icon-bus-y.png');
+    $('.service-icon>div:nth-child(2) a').css('color','#ffd800');
+  });
+  $('.service-icon>div:nth-child(2)').mouseleave(function(){
+    $('.service-icon>div:nth-child(2) img').attr('src','images/icon-bus.png');
+    $('.service-icon>div:nth-child(2) a').css('color','#fff');
+  });
+
+  $('.service-icon>div:nth-child(3)').mouseover(function(){
+    $('.service-icon>div:nth-child(3) img').attr('src','images/icon-tiket-y.png');
+    $('.service-icon>div:nth-child(3) a').css('color','#ffd800');
+  });
+  $('.service-icon>div:nth-child(3)').mouseleave(function(){
+    $('.service-icon>div:nth-child(3) img').attr('src','images/icon-tiket.png');
+    $('.service-icon>div:nth-child(3) a').css('color','#fff');
+  });
+
+  $('.service-icon>div:nth-child(4)').mouseover(function(){
+    $('.service-icon>div:nth-child(4) img').attr('src','images/icon-snap-y.png');
+    $('.service-icon>div:nth-child(4) a').css('color','#ffd800');
+  });
+  $('.service-icon>div:nth-child(4)').mouseleave(function(){
+    $('.service-icon>div:nth-child(4) img').attr('src','images/icon-snap.png');
+    $('.service-icon>div:nth-child(4) a').css('color','#fff');
+  });
+    
